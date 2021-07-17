7f45ed3c2a30b68917c781ceae681ae7d1f03b5c
davide <davide.baiocchi97@gmail.com>
2021-07-17 14:09:19 +0200

    end Jumbotron, with style scss

--- src/components/MainJumbotron.vue
+++ src/components/MainJumbotron.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="container">
-       
+        <div class="consulting">
+            <h3>Fusion & Acquisition</h3>
+            <h2>Insurance <br> Consulting</h2>
+            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Email">
+            <br>
+            <input type="text" placeholder="Phone">
+            <input type="text" placeholder="More info">
+            <br>
+            <button>get it touch</button>
+            <button class="btn-white">Read it more</button>
+        </div>
     </div>
 </template>
 
@@ -10,6 +21,6 @@ name : 'MainJumbotron'
 }
 </script>
 
-<style>
-
+<style scoped lang=scss>
+    @import '@/style/3mainJumbotron.scss';
 </style>
